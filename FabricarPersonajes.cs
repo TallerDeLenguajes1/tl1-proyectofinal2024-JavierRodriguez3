248236730b0fb42ef22f2c1240ebdee4dff69284
JavierRodriguez3 <javrodriguez2003@gmail.com>
2024-06-13 14:08:05 -0300

    Avances con la creacion de personajes, 2 enemgios completos, los mostre por pantalla y los guarde en el json

--- FabricarPersonajes.cs
+++ FabricarPersonajes.cs
@@ -5,24 +5,37 @@ namespace FabricarPersonajes;
 public class FabricaDePersonajes{
 private List<Personaje> enemigos;
 private Personaje miPersonaje;
-private string[] nombres;
 
 public FabricaDePersonajes(){
-    enemigos = new List<Personaje>();  
-    nombres = new string[] {"dangelo", "trigo", "targa", "fla", "guille", "luciano", "tommy", "peper"};
+    enemigos = new List<Personaje>(); 
 }
 public List<Personaje> CrearEnemigos(){
-
-    
+ 
     Random random = new Random();
+    Personaje[] NuevoPersonaje = new Personaje[2];
+    Array valoresRango = Enum.GetValues(typeof(RangoPersonaje));
+    
 
-    for (int i = 0; i < 8; i++)
+
+    for (int i = 0; i < NuevoPersonaje.Length; i++)
     {
-        Personaje NuevoPersonaje = new Personaje();
-        int indiceAlea = random.Next(nombres.Length);
-        NuevoPersonaje.Nombre=nombres[indiceAlea];
-        enemigos.Add(NuevoPersonaje);
+    RangoPersonaje RangoAleatorio = (RangoPersonaje)valoresRango.GetValue(random.Next(valoresRango.Length));
+
+        NuevoPersonaje[i] = new Personaje(){
+            Velocidad = random.Next(1, 11),
+            Destreza = random.Next(1, 6),
+            Fuerza = random.Next(1, 11),
+            Rango = RangoAleatorio,
+            Armadura = random.Next(1, 11),
+            Salud = 100,
+            Nombre = $"Kakashi Hatake {i}",
+            Apodo = $"El copiador {i}",
+            FechaNacimiento = DateTime.Parse("1995-7-2"),
+            Edad = random.Next(1, 301)
+        };
+        enemigos.Add(NuevoPersonaje[i]);
     }
+
     return enemigos;
 }
 
